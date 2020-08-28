2e420faa2afbafe52143666c7d999cf466023f85
Shirly <shirly1986@gmail.com>
2020-08-28 11:40:45 +0300

    new changes

--- apps/mail/cmps/EmailSideBar.jsx
+++ apps/mail/cmps/EmailSideBar.jsx
@@ -1,31 +1,34 @@
-import {eventBusService} from '../services/eventBusService.js'
+import { eventBusService } from '../services/eventBusService.js'
 import { emailService } from '../services/email-service.js'
-import {ComposeModal} from '../apps/mail/cmps/ComposeModal.jsx'
+import { ComposeModal } from '../apps/mail/cmps/ComposeModal.jsx'
 export class EmailSideBar extends React.Component {
 
-onToggleCompose =()=>{
-    eventBusService.emit('composeModal');
-    console.log('hi')
-}
+    onToggleCompose = () => {
+        eventBusService.emit('composeModal');
+        console.log('hi')
+    }
 
-countUnreadMail =() =>{
-    return emailService.unreadMailCount();
-}
+    countUnreadMail = () => {
+        return emailService.unreadMailCount();
+    }
 
-// onInBox=()=>{
-//     this.props.onSetFilter(null)
-// }
+    // onInBox=()=>{
+    //     this.props.onSetFilter(null)
+    // }
 
-// onSent =()=>{
-//     this.props.onSetFilter('sent')
-// }
+    // onSent =()=>{
+    //     this.props.onSetFilter('sent')
+    // }
 
-render(){
-    return <div className="side-bar">
-        <button onClick={this.onToggleCompose} className={'compose-button'}>Compose</button>
-        <ComposeModal></ComposeModal>
-    </div>
-}
+    render() {
+        return <div className="side-bar">
+            <button onClick={this.onToggleCompose} className={'compose-button'}><i class="fas fa-plus"></i></button>
+            <ComposeModal></ComposeModal>
+            <h3>Inbox</h3>
+            <h3>Sent Items</h3>
+            <h3>Deleted Items</h3>
+        </div>
+    }
 
 
 }
